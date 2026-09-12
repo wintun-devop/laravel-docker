337c8f57e34a4bd24ec7c1f8ca17e4dc39eae6a7
wintun-devop <wintun.hlaing@outlook.com>
2026-09-12 21:38:18 +0630

    basic api route success

--- ApiServer/bootstrap/app.php
+++ ApiServer/bootstrap/app.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/health.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
